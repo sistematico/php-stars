d5bf98cfb2e50c2119fc5204eefbcf45e907cf5a
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-02 22:24:01 -0400

    Autocommit por GitHub: alterações: add 'voto.php'

--- voto.php
+++ voto.php
@@ -1,22 +1,32 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $votos = json_decode(file_get_contents("votos.json"), true);
-    $totalStars = 0;
-    $voters = array_sum($votos);
-    foreach ($votos as $stars => $votes) {
-        $totalStars += $stars * $votes;
+
+$votos = json_decode(file_get_contents("votos.json"), true);
+$totalStars = 0;
+$voters = array_sum($votos);
+foreach ($votos as $stars => $votes) {
+    $totalStars += $stars * $votes;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['reset']) && !empty($_POST['reset'])) {
+        $votos = [
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0
+        ];
+
+        file_put_contents('votos.json', json_encode($votos));
     }
-    echo ($totalStars/$voters);
-} //else 
-//if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['voto']) && !empty($_POST['voto'])) {
-    $votos = json_decode(file_get_contents("votos.json"), true);
-    //extract($_POST);
-    $voto = ++$votos[$voto];
-    $votos[$voto] = $voto;
 
+    extract($_POST);
+    $novo = ++$votos[$voto];
+    $votos[$voto] = $novo;
     file_put_contents('votos.json', json_encode($votos));
-
     var_dump($votos);
+}
 
+echo ($totalStars/$voters);
 
 ?>
