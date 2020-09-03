ed0f91a9f30f6f5a1a51d4f316f1a582ebb5e297
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-02 22:27:01 -0400

    Autocommit por GitHub: alterações: add 'voto.php'

--- voto.php
+++ voto.php
@@ -1,15 +1,8 @@
 <?php
 
-$votos = json_decode(file_get_contents("votos.json"), true);
-$totalStars = 0;
-$voters = array_sum($votos);
-foreach ($votos as $stars => $votes) {
-    $totalStars += $stars * $votes;
-}
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['reset']) && !empty($_POST['reset'])) {
-        $votos = [
+        $reset = [
             1 => 0,
             2 => 0,
             3 => 0,
@@ -17,14 +10,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             5 => 0
         ];
 
-        file_put_contents('votos.json', json_encode($votos));
+        file_put_contents('votos.json', json_encode($reset));
     }
-
     extract($_POST);
     $novo = ++$votos[$voto];
     $votos[$voto] = $novo;
     file_put_contents('votos.json', json_encode($votos));
     var_dump($votos);
+}
+
+$votos = json_decode(file_get_contents("votos.json"), true);
+$totalStars = 0;
+$voters = array_sum($votos);
+foreach ($votos as $stars => $votes) {
+    $totalStars += $stars * $votes;
 }
 
 echo ($totalStars/$voters);
