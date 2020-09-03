35b12af35eacaf7bfe84e8ca07a08ee52523c997
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-09-02 22:06:01 -0400

    Autocommit por GitHub: alterações: add 'voto.php'

--- voto.php
+++ voto.php
@@ -10,13 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 } //else 
 //if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['voto']) && !empty($_POST['voto'])) {
     $votos = json_decode(file_get_contents("votos.json"), true);
-    
-    $key = 1;
-
-    $atual = $votos[$key];
+    //extract($_POST);
+    $voto = 1;
+    ++$votos[$voto];
 
     echo ++$atual;
 
-    //file_put_contents('votos.json', json_encode($ratings));
+    file_put_contents('votos.json', json_encode($ratings));
 
 ?>
